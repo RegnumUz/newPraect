4dd0e11b26afc30c61d7a9e86c251901e482998d
RegnumUz <regnum1308@gmail.com>
2022-02-27 15:21:48 +0500

    'newproect'

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Home from './page/home';
+import Card from './containers/cards/index.jsx'
+
+function App() {
+  return (
+    <div >
+      <Home /> 
+      <Card />
+    </div>
+  );
+}
+
+export default App;
